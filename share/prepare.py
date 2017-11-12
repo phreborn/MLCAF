77f9881d48aa39a00ce5c0545bc0657da1a3e969
David Shope <david.richard.shope@cern.ch>
2017-11-12 22:40:38 +0100

    Add some sanity output to the end of the prepare script

--- share/prepare.py
+++ share/prepare.py
@@ -25,15 +25,25 @@ def main(config):
     if config.getTagBoolDefault("printXsec",False):
         prepare.printXSecParser(samples)
 
-    # TODO: delete, just for debugging
-    # print "Exiting successfully!"
-    # sys.exit(0)
+    # make sure we have at least something in the sample folder
+    if samples.isEmpty():
+        QFramework.BREAK("your sample folder is empty. please make sure your configuration is sensible and try again!")
 
     # apply patches as given by the config
     common.patchSampleFolder(config.getTagVStandardString("patches"), samples)
 
+    # produce some output for the user to have a look at the result
+    QFramework.INFO("please investigate the top-level structure of your sample folder to make sure everything is in place!")
+    samples.printContents(config.getTagStringDefault("printFolderTags","r1dt"))
+
     # write the sample folder to disk
     common.writeSampleFolder(config, samples)
+
+    if config.hasUnreadKeys("!.*"):
+        QFramework.WARN("the following config keys were not read: "+(",".join([ key.GetName() for key in  config.getListOfUnreadKeys("!.*") ])))
+    #temporary fix to prevent segfaults in AnaBase 2.3.48 and beyond
+    # TODO: still necessary?
+    ROOT.xAOD.ClearTransientTrees()
 
 if __name__ == "__main__":
 
@@ -42,6 +52,7 @@ if __name__ == "__main__":
 
     import sys
     import QFramework
+    import ROOT
 
     # use the argument parser to read the command line arguments and config options from the config file
     config = common.getConfigOptions(parser.parse_args())
