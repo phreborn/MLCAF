683ad2a50c5ee77d4718b9fecbe799bfbd3fd8ab
Konstantin Lehmann <klehmann@sfu.ca>
2018-04-21 00:06:22 -0700

    First step towards updating the auto-complete function. Does not work yet.

--- setup/setupAutoComplete.sh
+++ setup/setupAutoComplete.sh
@@ -15,13 +15,11 @@ _hww_config_files(){
     previousWord=$3
 
 
-    # Check if $command is known (i.e. it is in $PATH)
-
+    # Check if $command is known
+    
     pathToCommand=$(which $command 2>/dev/null)
     # resolve leading "~/" and symbolic links
     pathToCommand="${pathToCommand/#\~/$HOME}"
-# doesn't work on all operating systems (i.e. doesn't work on Mac)
-#    pathToCommand="$(readlink -f $pathToCommand)"
     if [ ! -x "$pathToCommand" ] ; then
 	# $command does not point to an executable, exit script
 	return 0
@@ -29,15 +27,19 @@ _hww_config_files(){
 
 
     # Check if $command is in $CAFANALYSISSHARE. If not, exit the script.
-    
+    CAFAnalysisShare=""
     commandInExecutables=0
     # Loop over all directories in $CAFANALYSISSHARE
     for directory in `echo $CAFANALYSISSHARE | tr ":" " "`; do
 	# resolve leading "~/" and symbolic links
 	dir="${directory/#\~/$HOME}"
-# doesn't work on all operating system (i.e. doesn't work on Mac)
-#	dir="$(readlink -f $dir)/"
-
+	if [[ $dir != *"/" ]] ; then
+	    dir=$dir/
+	fi
+	if [[ $CAFAnalysisShare != "" ]] ; then
+	    CAFAnalysisShare+=" "
+	fi
+	CAFAnalysisShare+="$dir"
 	# The following two if statements are a messed up way of
 	# Checking that $command points to a script in $dir.
 
@@ -53,24 +55,83 @@ _hww_config_files(){
 	    fi
 	fi
     done
-
+    
     # Jump out of function, if executable is not in $CAFANALYSISSHARE
     if [ $commandInExecutables -eq 0 ] ; then
 	return 0
     fi
-
+    
     
     # Now, do the actual magic. Create a list of completions that are
     # going to be added to the standard suggestions.
     
-    # replace colons by spaces
-    hwwConfigPathFormatted=`echo $HWWCONFIGDIRECTORY | tr ":" " "`
 
-    # Make a list of all files in $HWWCONFIGDIRECTORY trailing with ".cfg"
-    listOfFiles="$(ls $hwwConfigPathFormatted |grep ".cfg$")"
+    # Find the directory by cutting after the last "/"
+    thisDir=""
+    if [[ $thisWord == *"/"* ]] ; then
+	thisDir=`echo $thisWord | rev | cut -d "/" -f2- | rev`
+    fi
+
+    # CAFAnalysisShare holds all paths separated by " "
     
-    # Compgen reduces the list to the words starting with $thisWord
-    COMPREPLY=( $(compgen -W "$listOfFiles" -- $thisWord) )
+    listOfFiles=""
+    listOfDirs=""
+    for path in $CAFAnalysisShare ; do
+	
+	thisPath="$path$thisDir"
+	if [[ $thisDir != "" ]] ; then
+	    thisPath+="/"
+	fi
+	listOfFiles_new="$(ls $thisPath 2>/dev/null |grep ".cfg$")"
+	listOfDirs_new="$(ls -d $thisPath*/ 2>/dev/null)"
+	for file in $listOfFiles_new  ; do
+	    if [[ $listOfFiles != "" ]] ; then
+		listOfFiles+=" "
+	    fi
+	    # cut off the absolute part of all suggestions and save in listOfFiles
+	    listOfFiles+=${file/$thisPath/}
+	done
+	for dir in $listOfDirs_new  ; do
+	    if [[ $listOfDirs != "" ]] ; then
+		listOfDirs+=" "
+	    fi
+	    # cut off the absolute part of all suggestions and save in listOfFiles
+	    listOfDirs+=${dir/$thisPath/}
+	done
+
+	# test if there is a subdirectory "config/master". if yes, write it into conlyCompletion. use it. otherwise, compile normal completions.
+	onlyCompletion=""
+	for dir in $listOfDirs ; do
+	    if [[ $dir == *"config/" ]] ; then
+		listOfDirsInConfig="$(ls -d $thisPath$dir*/)"
+		for dirInConfig in $listOfDirsInConfig ; do
+		    if [[ $dirInConfig == *"config/master/" ]] ; then
+			onlyCompletion=$thisWord$dir"master"
+		    fi
+		done
+	    fi
+	done
+	if [[ $onlyCompletion != "" ]] ; then	    
+	    echo "complete to $onlyCompletion"
+	fi
+    done
+
+
+    
+    return 0
+
+    if [[ $listOfDirs == "master/"* ]] ; then
+	if [[ "$(ls -d ($thisDir)master/)" == *"config/"* ]] ; then
+	    echo "make cool completion"
+	    someVar=1
+	fi
+    fi
+    if [ $someVar -eq 0 ] ; then
+	echo "make not so cool completion"
+    fi
+    
+    # # Compgen reduces the list to the words starting with $thisWord
+    # COMPREPLY=( $(compgen -W "$listOfFiles" -- $thisWord) )
     
     return 0
 }
