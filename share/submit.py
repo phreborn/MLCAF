40624b6e0e327cd0eb3170cba9939939a12aa621
rgugel <rgugel@cern.ch>
2018-04-21 16:11:30 +0200

    some ifrst work on submission script for CAFExample

--- share/submit.py
+++ share/submit.py
@@ -4,6 +4,7 @@ from CommonAnalysisHelpers import common,submit
 
 def main(config):
     """submit your analysis to a batch system"""
+    print(config)
     print("hello")
 
 
@@ -11,13 +12,14 @@ if __name__ == "__main__":
 
     # create a pre-configured argument parser
     parser = submit.DefaultArgumentParser()
-
+    
+    # use the argument parser to read the command line arguments and config options from the config file
+    config = common.getConfigOptions(parser.parse_args())
+    
     import QFramework
     import ROOT
 
-    # use the argument parser to read the command line arguments and config options from the config file
-    config = common.getConfigOptions(parser.parse_args())
-
+    
     # ignore command line arguments since ROOT is very greedy here (and tends to choke from it!)
     ROOT.PyConfig.IgnoreCommandLineOptions = True
 
