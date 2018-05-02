0c8b6a2c6e3a7a53368b63653c039e74c84dae55
Carsten Burgard <cburgard@cern.ch>
2018-05-03 00:52:27 +0200

    python3 compatibility

--- share/flatNTuple/observables/FakeLeptonMT.py
+++ share/flatNTuple/observables/FakeLeptonMT.py
@@ -1,12 +1,12 @@
-from QFramework import *
-from ROOT import *
+import QFramework
+import ROOT
 
 from CAFExample import FakeLeptonMT
 
 def addObservables():
 
   myObs = FakeLeptonMT("FakeLeptonMT")
-  if not TQTreeObservable.addObservable(myObs, "FakeLeptonMT"):
+  if not QFramework.TQTreeObservable.addObservable(myObs, "FakeLeptonMT"):
     return False
 
   return True
