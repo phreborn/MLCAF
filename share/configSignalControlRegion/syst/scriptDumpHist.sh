3bd584a1627d32e7e984eb0b4ef2b830ed2c6c87
Theodore Zorbas <t.zorbas@cern.ch>
2019-04-02 12:56:11 +0100

    hadd histograms

--- share/configSignalControlRegion/syst/scriptDumpHist.sh
+++ share/configSignalControlRegion/syst/scriptDumpHist.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# dump histograms to ROOT files
 for systematic in $(ls -1 sampleFolders/analyzed/samples-analyzed-htautau_lephad_sr-*.root | grep -v "sys_band"); do
     for campaign in mc16a; do
         for channel in ehad muhad; do
             python scripts/dumpHist.py $systematic $campaign $channel
         done
+    done
+done
+
+# hadd histograms to common files
+for campaign in mc16a; do
+    for cut in wcr1pBtag wcr1pBveto wcr3pBtag wcr3pBveto vr1pBtag vr1pBveto vr3pBtag vr3pBveto tcr1pBtag tcr3pBtag; do
+        hadd -n 11 -f dumpHist/${campaign}_${cut}.root dumpHist/*/${campaign}/*/${campaign}_*_${cut}_*.root
     done
 done
