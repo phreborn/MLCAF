d1f686a2046ff72d9767d971de73c25a536e84d0
Theodore Zorbas <t.zorbas@cern.ch>
2019-05-21 14:54:25 +0100

    Quick apply FF tags in LFR

--- share/configLeptonFakeRegion/applyFF/scriptPrepareInitialize.sh
+++ share/configLeptonFakeRegion/applyFF/scriptPrepareInitialize.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-initialize.py configLeptonFakeRegion/applyFF/htautau_lephad_lfr_applyff.cfg
+tqmerge -o sampleFolders/initialized/samples-initialized-htautau_lephad_lfr_applyff.root -t initialize sampleFolders/initialized/samples-initialized-htautau_lephad_lfr.root -p configLeptonFakeRegion/applyFF/htautau_lephad_lfr_applyff.tags
