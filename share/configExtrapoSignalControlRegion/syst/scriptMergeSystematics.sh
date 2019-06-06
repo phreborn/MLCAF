20d8b440f184e0747317a6671b5aa7d43084aeb2
Xiaozhong Huang <xiaozhong.huang@cern.ch>
2019-06-07 00:26:40 +0800

    remove configExtrapoSignalControlRegion

--- share/configExtrapoSignalControlRegion/syst/scriptMergeSystematics.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python configExtrapoSignalControlRegion/syst/submit_systematics.py --systype fakevar --merge
-python configExtrapoSignalControlRegion/syst/submit_systematics.py --systype isovar --merge
-python configExtrapoSignalControlRegion/syst/submit_systematics.py --systype ttbarweight --merge
-python configExtrapoSignalControlRegion/syst/submit_systematics.py --systype weightvar --merge
-python configExtrapoSignalControlRegion/syst/submit_systematics.py --systype treevariation --merge
-python configExtrapoSignalControlRegion/syst/submit_systematics.py --systype extraposf --merge
