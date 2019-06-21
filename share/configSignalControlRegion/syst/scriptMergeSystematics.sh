5d087c882a1cd3bd2ea29e7742258914d5b9be93
Theodore Zorbas <t.zorbas@cern.ch>
2019-06-21 14:14:17 +0100

    Fixes for systematics

--- share/configSignalControlRegion/syst/scriptMergeSystematics.sh
+++ share/configSignalControlRegion/syst/scriptMergeSystematics.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
 NCORES=1
+
 python configSignalControlRegion/syst/submit_systematics.py --systype fakevar --stage merge --ncores ${NCORES}
 python configSignalControlRegion/syst/submit_systematics.py --systype isovar --stage merge --ncores ${NCORES}
+
 python configSignalControlRegion/syst/submit_systematics.py --systype extraposf --stage merge --ncores ${NCORES}
-python configSignalControlRegion/syst/submit_systematics.py --systype treevariation --stage merge --ncores ${NCORES}
-python configSignalControlRegion/syst/submit_systematics.py --systype ttbarweight --stage merge --ncores ${NCORES}
-python configSignalControlRegion/syst/submit_systematics.py --systype weightvar --stage merge --ncores ${NCORES}
+
+#python configSignalControlRegion/syst/submit_systematics.py --systype ttbarweight --stage merge --ncores ${NCORES}
+
+#python configSignalControlRegion/syst/submit_systematics.py --systype weightvar --stage merge --ncores ${NCORES}
+#python configSignalControlRegion/syst/submit_systematics.py --systype treevariation --stage merge --ncores ${NCORES}
--- share/configSignalControlRegion/syst/scriptSubmitSystematics.sh
+++ share/configSignalControlRegion/syst/scriptSubmitSystematics.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 NCORES=1
+
 python configSignalControlRegion/syst/submit_systematics.py --systype isovar --stage analyze --ncores ${NCORES}
 python configSignalControlRegion/syst/submit_systematics.py --systype fakevar --stage analyze --ncores ${NCORES}
+
 python configSignalControlRegion/syst/submit_systematics.py --systype extraposf --stage analyze --ncores ${NCORES}
-python configSignalControlRegion/syst/submit_systematics.py --systype treevariation --stage analyze --ncores ${NCORES}
-python configSignalControlRegion/syst/submit_systematics.py --systype weightvar --stage analyze --ncores ${NCORES}
-python configSignalControlRegion/syst/submit_systematics.py --systype ttbarweight --stage analyze --ncores ${NCORES}
+
+#python configSignalControlRegion/syst/submit_systematics.py --systype ttbarweight --stage analyze --ncores ${NCORES}
+
+#python configSignalControlRegion/syst/submit_systematics.py --systype weightvar --stage analyze --ncores ${NCORES}
+#python configSignalControlRegion/syst/submit_systematics.py --systype treevariation --stage analyze --ncores ${NCORES}
