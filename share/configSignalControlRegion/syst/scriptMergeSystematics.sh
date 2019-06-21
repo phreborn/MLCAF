2158768404b1544cdc784e9ed9c0c786a5810da9
Xiaozhong Huang <xiaozhong.huang@cern.ch>
2019-06-21 12:59:05 +0800

    align up the systematic naming skeme

--- share/configSignalControlRegion/syst/scriptMergeSystematics.sh
+++ share/configSignalControlRegion/syst/scriptMergeSystematics.sh
@@ -2,7 +2,6 @@
 NCORES=1
 python configSignalControlRegion/syst/submit_systematics.py --systype fakevar --stage merge --ncores ${NCORES}
 python configSignalControlRegion/syst/submit_systematics.py --systype isovar --stage merge --ncores ${NCORES}
-python configSignalControlRegion/syst/submit_systematics.py --systype extraposf --stage merge --ncores ${NCORES}
-python configSignalControlRegion/syst/submit_systematics.py --systype treevariation --stage merge --ncores ${NCORES}
-python configSignalControlRegion/syst/submit_systematics.py --systype ttbarweight --stage merge --ncores ${NCORES}
+python configSignalControlRegion/syst/submit_systematics.py --systype treevar --stage merge --ncores ${NCORES}
+python configSignalControlRegion/syst/submit_systematics.py --systype topvar --stage merge --ncores ${NCORES}
 python configSignalControlRegion/syst/submit_systematics.py --systype weightvar --stage merge --ncores ${NCORES}
