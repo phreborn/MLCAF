6a9f08ac10658fb2909c12b0ae4bf7b10160330d
Xiaozhong Huang <xiaozhong.huang@cern.ch>
2019-06-21 14:56:04 +0800

    Reweight Top in Btag category

---
+++ BSMtautauCAF/TopReweight.h
@@ -0,0 +1,50 @@
+//this file looks like plain C, but it's actually -*- c++ -*-
+#ifndef __TOPREWEIGHT__
+#define __TOPREWEIGHT__
+
+#include "BSMtautauCAF/LepHadObservable.h"
+
+class TopReweight : public LepHadObservable {
+protected:
+  // put here any data members your class might need
+
+public:
+  virtual double getValue() const override;
+  virtual TObjArray* getBranchNames() const override;
+protected:
+  virtual bool initializeSelf() override;
+  virtual bool finalizeSelf() override;
+protected:
+
+  TString fExpression = "";
+
+  TString fSysName = "";
+
+  bool _isData = false;
+  TDirectory* m_histoDir = 0;
+
+  const long LIMIT_2016 = 311563;
+  const long LIMIT_2017 = 341649;
+  const long LIMIT_2018 = 364292;
+
+  std::map<TString, TH1F*> m_SF_hist;
+
+public:
+  virtual bool hasExpression() const override;
+  virtual const TString& getExpression() const override;
+  virtual void setExpression(const TString& expr) override;
+
+  TopReweight();
+  TopReweight(const TString& expression);
+  virtual ~TopReweight();
+public:
+  bool parseExpression(const TString& expr);
+  void clearParsedExpression();
+
+  virtual TString getActiveExpression() const override;
+
+  ClassDef(TopReweight, 1);
+
+
+};
+#endif
