ba93a2b1064947864fc718b373023f2ee7da089d
Hanfei Ye <hanfei.ye@cern.ch>
2019-06-25 11:19:40 +0200

    Update FailID SR folder

--- share/configFailIDSignalControlRegion/scriptSubmit.sh
+++ share/configFailIDSignalControlRegion/scriptSubmit.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# add "applyff"/"contid" as argument to executing this script to run over applyff/contid
+# add "contid" as argument to executing this script to run over contid
 
 REGION="configFailIDSignalControlRegion"
 JOBS="jobsALL.txt"
-CONFIG="htautau_lephad_sr_failID"
-IDENT="SR"
+CONFIG="htautau_lephad_sr_lffcheck"
+IDENT="SR_lffcheck"
 
-if [ "$1" == "applyff" ]; then
-  CONFIG="${CONFIG}_applyff"
-  IDENT="${IDENT}applyff"
-fi
 if [ "$1" == "contid" ]; then
   CONFIG="${CONFIG}_contid"
   IDENT="${IDENT}contid"
--- share/configFailIDSignalControlRegion/scriptVisualize.sh
+++ share/configFailIDSignalControlRegion/scriptVisualize.sh
@@ -1,47 +1,29 @@
 #!/bin/bash
 
 ### WCR
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_wcr_mc16a.cfg
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_wcr_mc16c.cfg
+#visualize.py configFailIDSignalControlRegion/htautau_lephad_wcr_lffcheck.cfg
+#source scripts/tqpdflatex.sh results/htautau_lephad_wcr_lffcheck/summary.tex
 
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_wcr_wjets_mc16a.cfg
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_wcr_wjets_mc16c.cfg
-
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_wcr_qcd_mc16a.cfg
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_wcr_qcd_mc16c.cfg
-
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_wcr_wjetsqcd_mc16a.cfg
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_wcr_wjetsqcd_mc16c.cfg
-
+#visualize.py configFailIDSignalControlRegion/htautau_lephad_wcr_lffcheck_wjets.cfg
+#visualize.py configFailIDSignalControlRegion/htautau_lephad_wcr_lffcheck_qcd.cfg
+#visualize.py configFailIDSignalControlRegion/htautau_lephad_wcr_lffcheck_wjetsqcd.cfg
 
 ### TCR
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_tcr_mc16a.cfg
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_tcr_mc16c.cfg
+#visualize.py configFailIDSignalControlRegion/htautau_lephad_tcr_lffcheck.cfg
+#source scripts/tqpdflatex.sh results/htautau_lephad_tcr_lffcheck/summary.tex
 
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_tcr_wjets_mc16a.cfg
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_tcr_wjets_mc16c.cfg
-
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_tcr_qcd_mc16a.cfg
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_tcr_qcd_mc16c.cfg
-
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_tcr_wjetsqcd_mc16a.cfg
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_tcr_wjetsqcd_mc16c.cfg
-
+#visualize.py configFailIDSignalControlRegion/htautau_lephad_tcr_lffcheck_wjets.cfg
+#visualize.py configFailIDSignalControlRegion/htautau_lephad_tcr_lffcheck_qcd.cfg
+#visualize.py configFailIDSignalControlRegion/htautau_lephad_tcr_lffcheck_wjetsqcd.cfg
 
 ### VR
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_vr_mc16a.cfg
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_vr_mc16c.cfg
+visualize.py configFailIDSignalControlRegion/htautau_lephad_vr_lffcheck.cfg
+source scripts/tqpdflatex.sh results/htautau_lephad_vr_lffcheck/summary.tex
 
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_vr_wjets_mc16a.cfg
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_vr_wjets_mc16c.cfg
-
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_vr_qcd_mc16a.cfg
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_vr_qcd_mc16c.cfg
-
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_vr_wjetsqcd_mc16a.cfg
-#visualize.py configFailIDSignalControlRegion/htautau_lephad_vr_wjetsqcd_mc16c.cfg
-
+#visualize.py configFailIDSignalControlRegion/htautau_lephad_vr_lffcheck_wjets.cfg
+#visualize.py configFailIDSignalControlRegion/htautau_lephad_vr_lffcheck_qcd.cfg
+#visualize.py configFailIDSignalControlRegion/htautau_lephad_vr_lffcheck_wjetsqcd.cfg
 
 ### SR
-visualize.py configFailIDSignalControlRegion/htautau_lephad_sr_failID_mc16a.cfg
-visualize.py configFailIDSignalControlRegion/htautau_lephad_sr_failID_mc16c.cfg
+#visualize.py configFailIDSignalControlRegion/htautau_lephad_sr_lffcheck.cfg
+#source scripts/tqpdflatex.sh results/htautau_lephad_sr_lffcheck/summary.tex
