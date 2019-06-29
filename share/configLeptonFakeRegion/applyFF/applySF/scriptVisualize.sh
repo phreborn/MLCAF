4ed799bbfcf375238f15942e6f61457f8d9d1fc1
Xiaozhong Huang <xiaozhong.huang@cern.ch>
2019-06-29 22:18:28 +0800

    Do not plot qcd component in LFR by default

--- share/configLeptonFakeRegion/applyFF/applySF/scriptVisualize.sh
+++ share/configLeptonFakeRegion/applyFF/applySF/scriptVisualize.sh
@@ -3,5 +3,5 @@
 visualize.py configLeptonFakeRegion/applyFF/applySF/htautau_lephad_lfr_applyffsf.cfg
 source scripts/tqpdflatex.sh results/htautau_lephad_lfr_applyffsf/summary.tex
 
-visualize.py configLeptonFakeRegion/applyFF/applySF/htautau_lephad_lfr_applyffsf_qcd.cfg
-source scripts/tqpdflatex.sh results/htautau_lephad_lfr_applyffsf_qcd/summary.tex
+#visualize.py configLeptonFakeRegion/applyFF/applySF/htautau_lephad_lfr_applyffsf_qcd.cfg
+#source scripts/tqpdflatex.sh results/htautau_lephad_lfr_applyffsf_qcd/summary.tex
