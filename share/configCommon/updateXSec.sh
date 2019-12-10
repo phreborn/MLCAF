b303a77efa15c6db7f1ce9ff49063b13ec37f6fa
Theodore Zorbas <t.zorbas@cern.ch>
2019-12-10 18:14:48 +0000

    Re-set all signal cross-sections to 1.0 pb

--- share/configCommon/updateXSec.sh
+++ share/configCommon/updateXSec.sh
@@ -7,3 +7,6 @@ DSIDS=$(grep -F '/$(channel)/' configCommon/htautau_lephad_common.map | cut -d '
 grep -wF "${DSIDS}" "${PMG_XSEC_FILE}" | sort | sed '1i SampleID process xsectioninpb filtereff kfactor uncertup uncertdown generator etag' \
     | tr -s '[:blank:]' '~' | sed 's/~/,~/g' | column -t -s '~' | sed 's/\( *\) /\1/g' \
         > configCommon/htautau_lephad_common_xsec.csv-$(date +"%m-%d-%Y")
+
+echo "then manually edit all bbH/ggH cross-sections to equal 1.0 pb"
+echo "or find a built-in solution in the framework to modify it at runtime"
