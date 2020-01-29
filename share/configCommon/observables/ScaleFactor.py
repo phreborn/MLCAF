75d4cc0d20387f80ddcc590ad8fda4a6946d6616
Xiaozhong Huang <xiaozhong.huang@cern.ch>
2020-01-30 01:51:28 +0800

    remove unused observable

--- share/configCommon/observables/ScaleFactor.py
+++ share/configCommon/observables/ScaleFactor.py
@@ -3,12 +3,9 @@ import ROOT
 from BSMtautauCAF import ScaleFactor
 
 def addObservables(config):
-    weightSystematic = config.getTagStringDefault("weightvar","nominal")
-    names = ["ScaleFactor"]
-    names += ["ScaleFactor_%s" % weightSystematic]
-    for name in names:
-        myObs = ScaleFactor(name)
-        if not QFramework.TQTreeObservable.addObservable(myObs,name):
-            INFO("failed to add " + name + " Observable!")
-            return False
+    variation = config.getTagStringDefault("weightvar","nominal")
+    myObs = ScaleFactor("ScaleFactor_"+variation)
+    if not QFramework.TQTreeObservable.addObservable(myObs, "ScaleFactor_"+variation):
+        INFO("failed to add " + name + " Observable!")
+        return False
     return True
