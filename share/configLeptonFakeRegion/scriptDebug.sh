8eb790c0ec57143109e63978d0f5e4cb9f679584
Theodore Zorbas <t.zorbas@cern.ch>
2020-01-29 13:44:45 +0000

    Increase LFR debug snowflake threshold

--- share/configLeptonFakeRegion/scriptDebug.sh
+++ share/configLeptonFakeRegion/scriptDebug.sh
@@ -5,7 +5,7 @@ if [ "${GITLAB_CI}" == "true" ]; then IS_GITLAB_CI="true"; else IS_GITLAB_CI="fa
 CI_OPTS=""
 if [ "${IS_GITLAB_CI}" == "true" ]; then
     echo "Applying CI exceptions..."
-    CI_OPTS="--options lineUpdates=false consoleWidth=160 --snowflakeThresh 2"
+    CI_OPTS="--options lineUpdates=false consoleWidth=160 --snowflakeThresh 3"
 fi
 
 analyze.py configLeptonFakeRegion/htautau_lephad_lfr.cfg --debug --restrict /bkg/?/c16a/Top/single ${CI_OPTS}
