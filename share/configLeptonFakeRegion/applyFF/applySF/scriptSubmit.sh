fe9f7c33d59ed0d689eed2723347c912f20344d5
Theodore Zorbas <t.zorbas@cern.ch>
2020-01-30 17:07:16 +0000

    Apply new jobLists scheme to LFR

--- share/configLeptonFakeRegion/applyFF/applySF/scriptSubmit.sh
+++ share/configLeptonFakeRegion/applyFF/applySF/scriptSubmit.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# add "contid" as argument to executing this script to run over contid
-
+# config
 REGION="configLeptonFakeRegion/applyFF/applySF"
-JOBS="jobsLFR_applyFFSF.txt"
 CONFIG="htautau_lephad_lfr_applyffsf"
 IDENT="LFRapplyFFSF"
+JOBSLIST=(
+    "fakesISO"
+)
 
-if [ "$1" == "contid" ]; then
-  CONFIG="${CONFIG}_contid"
-  IDENT="${IDENT}contid"
-fi
-
-source configCommon/scriptSubmit.sh "${REGION}" "${CONFIG}" "${JOBS}" "${IDENT}"
+# submit
+source configCommon/scriptSubmit.sh "${REGION}" "${CONFIG}" "${IDENT}" "${JOBSLIST}"
