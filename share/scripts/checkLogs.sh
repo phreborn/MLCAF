9bf585676c5deee5db5b3d0561eb04b1a610289a
Theodore Zorbas <t.zorbas@cern.ch>
2020-02-07 12:32:34 +0000

    Ignore warning/error summary counts in log check

--- share/scripts/checkLogs.sh
+++ share/scripts/checkLogs.sh
@@ -6,4 +6,5 @@ find ./logs -type f -name "*" | xargs grep -iEr "warn|fail|error" \
     | grep -v "systematics handling" \
     | grep -vE "root does not exist|can not find SF" | sed -e "/No cuts were activated for processing sample/,+1d" \
     | grep -vE "unexpected pileup weight|atlas-nightlies /cvmfs/atlas-nightlies.cern.ch/repo may be stale" \
+    | grep -v '\[1;33mWARNING\[0m\[1m: \[0m\[1m   ' | grep -v '\[1;31mERROR\[0m\[1m: \[0m\[1m   ' \
     | uniq | sort | uniq;
