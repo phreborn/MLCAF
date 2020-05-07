68c473eeefdc325be15b32e894fd3c637a7f33e6
Hanfei Ye <hanfei.ye@cern.ch>
2020-05-07 10:55:52 +0800

    remove observable InvMTauJet but take it as a alias. It's now easy to calculate invariant mass since jet info is stored as LorentzVector.

--- share/lqtaub_lephad/observables/InvMTauJet.py
+++
@@ -1,9 +0,0 @@
-import QFramework
-from BSMtautauCAF import InvMTauJet
-
-def addObservables(config):
-    myObs = InvMTauJet("InvMTauJet")
-    if not QFramework.TQTreeObservable.addObservable(myObs, myObs.getExpression().Data()):
-        QFramework.WARN("Failed to add observable '{:s}'".format(myObs.getExpression().Data()))
-        return False
-    return True
