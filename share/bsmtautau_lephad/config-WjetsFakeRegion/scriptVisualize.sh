30d644a306cebe1360f9f2f07fe176784c278180
Xiaozhong Huang <xiaozhong.huang@cern.ch>
2020-07-24 10:21:50 +0800

    add SS to WFR

--- share/bsmtautau_lephad/config-WjetsFakeRegion/scriptVisualize.sh
+++ share/bsmtautau_lephad/config-WjetsFakeRegion/scriptVisualize.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-visualize.py bsmtautau_lephad/config-WjetsFakeRegion/master/master.cfg
-source "${CAFANALYSISSHARE:?}/scripts/tqpdflatex.sh" results/bsmtautau_lephad_wfr/summary.tex
+visualize.py bsmtautau_lephad/config-WjetsFakeRegion/master/master_os.cfg
+source "${CAFANALYSISSHARE:?}/scripts/tqpdflatex.sh" results/bsmtautau_lephad_os_wfr/summary.tex
+
+visualize.py bsmtautau_lephad/config-WjetsFakeRegion/master/master_ss.cfg
+source "${CAFANALYSISSHARE:?}/scripts/tqpdflatex.sh" results/bsmtautau_lephad_ss_wfr/summary.tex
