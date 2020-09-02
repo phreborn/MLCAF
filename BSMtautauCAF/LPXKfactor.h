137ec71616ddb55558508a6c0be3a8ad4dec8ef0
Xiaozhong Huang <xiaozhong.huang@cern.ch>
2020-09-03 06:12:19 +0800

    Use the LPXKfactor stored in lephad ntuples

--- BSMtautauCAF/LPXKfactor.h
+++ BSMtautauCAF/LPXKfactor.h
@@ -1,9 +1,15 @@
 //this file looks like plain C, but it's actually -*- c++ -*-
-#ifndef __LPXKFACTOR__
-#define __LPXKFACTOR__
+#ifndef __LPXKFACTORNEW__
+#define __LPXKFACTORNEW__
 
 #include "BSMtautauCAF/LepHadObservable.h"
 #include "TGraphAsymmErrors.h"
+
+#include <tuple>
+#include <bitset>
+#include <vector>
+#include "TTreeFormula.h"
+#include "TH2.h"
 
 class LPXKfactor : public LepHadObservable {
 
@@ -14,21 +20,28 @@ class LPXKfactor : public LepHadObservable {
     LPXKfactor(const TString& expression);
     ~LPXKfactor();
 
-    virtual bool hasExpression() const override;
-    virtual const TString& getExpression() const override;
-    virtual void setExpression(const TString& expr) override;
     virtual double getValue() const override;
     virtual TObjArray* getBranchNames() const override;
 
   protected:
     virtual bool initializeSelf() override;
     virtual bool finalizeSelf() override;
+    
+    void addScaleFactor(Condition requirement, TString branch);
+    void addScaleFactor(Condition requirement, Condition veto, TString branch);
+    Condition registerVariation(TString name);
+    
     TString fExpression = "";
     TString fSysName = "";
-    TDirectory* m_histoDir = 0;
 
-    std::map<int, int> m_DSID_bin;
-    std::map<TString, TGraphAsymmErrors*> m_SF_graph;
-
+    std::vector< std::pair<TString, Condition> > m_variations;
+    std::vector< std::tuple<Condition, Condition, TString, TTreeFormula*> > m_branches;
+    Condition m_variation;
+    
+    const Condition none  = 0;
+    const Condition nominal = 1<<0;
+    unsigned int m_nextBitPosition = 1; // position to register new variations
+    Condition m_staticConditionsMask; // bit including all the variations
+    
 };
 #endif
