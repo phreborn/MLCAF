83591ec77980aeecbde4b370726c92899e0b35b0
Xiaozhong Huang <xiaozhong.huang@cern.ch>
2021-05-17 11:15:43 +0800

    fix the failure of compilation

--- BSMtautauCAF/LepHadObservable.h
+++ BSMtautauCAF/LepHadObservable.h
@@ -58,8 +58,10 @@ protected:
   const long LIMIT_2017 = 341649;
   const long LIMIT_2018 = 364292;
 
-  bool _isData = false;
-  bool _isMC = false;
+  bool m_isData = false;
+  bool m_isMC = false;
+
+  bool m_verbose = false;
 
   TTreeFormula* n_bjets = nullptr;
   TTreeFormula* lep_0_iso_electron = nullptr;
