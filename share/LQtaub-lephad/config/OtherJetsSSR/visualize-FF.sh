b28c13bb3e473c9a4fa27f788a943ef392b2ef83
Hanfei Ye <hanfei.ye@cern.ch>
2021-11-14 13:50:53 +0800

    update OtherJetsSSR visualization

--- share/LQtaub-lephad/config/OtherJetsSSR/visualize-FF.sh
+++ share/LQtaub-lephad/config/OtherJetsSSR/visualize-FF.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 REGION="OtherJetsSSR"
-TASK="FF-ss"
+TASK="FF"
 visualize.py LQtaub-lephad/config/${REGION}/master/visualize-${TASK}.cfg 
 source "${CAFANALYSISSHARE:?}/scripts/tqpdflatex.sh" results/LQtaub-lephad-${REGION}-${TASK}/summary.tex
