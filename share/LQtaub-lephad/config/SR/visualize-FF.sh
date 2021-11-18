2610641a9374b09827c19f1cb316cf5f2539e9fe
Hanfei Ye <hanfei.ye@cern.ch>
2021-11-18 19:19:57 +0800

    update histograms and visualization in TCR and SR

--- share/LQtaub-lephad/config/SR/visualize-FF.sh
+++ share/LQtaub-lephad/config/SR/visualize-FF.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#REGION="SR"
-#TASK="FF-os"
-#visualize.py LQtaub-lephad/config/${REGION}/master/visualize-${TASK}.cfg 
-#source "${CAFANALYSISSHARE:?}/scripts/tqpdflatex.sh" results/LQtaub-lephad-${REGION}-${TASK}/summary.tex
+REGION="SR"
+TASK="FF-os"
+visualize.py LQtaub-lephad/config/${REGION}/master/visualize-${TASK}.cfg 
+source "${CAFANALYSISSHARE:?}/scripts/tqpdflatex.sh" results/LQtaub-lephad-${REGION}-${TASK}/summary.tex
 
 REGION="VR"
 TASK="FF-os"
