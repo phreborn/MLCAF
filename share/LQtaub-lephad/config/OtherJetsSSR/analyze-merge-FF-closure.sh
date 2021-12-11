878de13fecf98c3a20d58a5502c762c09892b19d
Hanfei Ye <hanfei.ye@cern.ch>
2021-12-11 15:23:16 +0800

    update OtherJetsSSR

--- share/LQtaub-lephad/config/OtherJetsSSR/analyze-merge-FF-closure.sh
+++ share/LQtaub-lephad/config/OtherJetsSSR/analyze-merge-FF-closure.sh
@@ -3,5 +3,4 @@
 REGION="OtherJetsSSR"
 TASK="FF-closure"
 
-tqmerge -o sampleFolders/analyzed/samples-analyzed-LQtaub-lephad-${REGION}-${TASK}.root -t analyze batchOutput/unmerged_${REGION}-${TASK}/*.root \
-    sampleFolders/analyzed/samples-analyzed-LQtaub-lephad-OtherJetsSSR-FF.root
+tqmerge -o sampleFolders/analyzed/samples-analyzed-LQtaub-lephad-${REGION}-${TASK}.root -t analyze batchOutput/unmerged_${REGION}-${TASK}/*.root
--- share/LQtaub-lephad/config/OtherJetsSSR/analyze-submit-FF-closure.sh
+++ share/LQtaub-lephad/config/OtherJetsSSR/analyze-submit-FF-closure.sh
@@ -10,6 +10,7 @@ CONFIG="LQtaub-lephad/config/${REGION}/master/analyze-${TASK}.cfg"
 IDENTIFIER=${REGION}-${TASK}
 
 # Job list
-JOBLIST="jobs_${REGION}-${TASK}_c16a.txt jobs_${REGION}-${TASK}_c16d.txt jobs_${REGION}-${TASK}_c16e.txt"
+#JOBLIST="jobs_${REGION}-${TASK}_c16a.txt jobs_${REGION}-${TASK}_c16d.txt jobs_${REGION}-${TASK}_c16e.txt"
+JOBLIST="jobs_${REGION}-FF_c16a.txt jobs_${REGION}-FF_c16d.txt jobs_${REGION}-FF_c16e.txt"
 
 source "${CAFANALYSISSHARE:?}/LQtaub-lephad/config/common/submit.sh" "${CONFIG}" "${IDENTIFIER}" "${JOBLIST}"
