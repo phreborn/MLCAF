5bc3b41cf5828a44acdf4578ac9724623e579a9f
Hanfei Ye <hanfei.ye@cern.ch>
2021-12-25 15:56:22 +0800

    update TCR

--- share/LQtaub-lephad/config/TCR/visualize-TCR-SF.sh
+++ share/LQtaub-lephad/config/TCR/visualize-TCR-SF.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#TASK="TCR-SF"
-#visualize.py LQtaub-lephad/config/TCR/master/visualize-${TASK}.cfg 
-#source "${CAFANALYSISSHARE:?}/scripts/tqpdflatex.sh" results/LQtaub-lephad-${TASK}/summary.tex
+TASK="TCR-SF"
+visualize.py LQtaub-lephad/config/TCR/master/visualize-${TASK}.cfg 
+source "${CAFANALYSISSHARE:?}/scripts/tqpdflatex.sh" results/LQtaub-lephad-${TASK}/summary.tex
 
 #TASK="TCR-PS-SF"
 #visualize.py LQtaub-lephad/config/TCR/master/visualize-${TASK}.cfg 
@@ -12,9 +12,9 @@
 #visualize.py LQtaub-lephad/config/TCR/master/visualize-${TASK}.cfg 
 #source "${CAFANALYSISSHARE:?}/scripts/tqpdflatex.sh" results/LQtaub-lephad-${TASK}/summary.tex
 
-TASK="TCR-ISRup-SF"
-visualize.py LQtaub-lephad/config/TCR/master/visualize-${TASK}.cfg 
-source "${CAFANALYSISSHARE:?}/scripts/tqpdflatex.sh" results/LQtaub-lephad-${TASK}/summary.tex
+#TASK="TCR-ISRup-SF"
+#visualize.py LQtaub-lephad/config/TCR/master/visualize-${TASK}.cfg 
+#source "${CAFANALYSISSHARE:?}/scripts/tqpdflatex.sh" results/LQtaub-lephad-${TASK}/summary.tex
 
 #TASK="TCR-ISRdo-SF"
 #visualize.py LQtaub-lephad/config/TCR/master/visualize-${TASK}.cfg 
