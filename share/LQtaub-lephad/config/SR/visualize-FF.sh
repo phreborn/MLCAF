4050c9cea9a023bf59e3a36ba70805456a2c44c3
Hanfei Ye <hanfei.ye@cern.ch>
2022-01-17 14:42:28 +0800

    update SR

--- share/LQtaub-lephad/config/SR/visualize-FF.sh
+++ share/LQtaub-lephad/config/SR/visualize-FF.sh
@@ -5,12 +5,7 @@ TASK="FF-os"
 visualize.py LQtaub-lephad/config/${REGION}/master/visualize-${TASK}.cfg 
 source "${CAFANALYSISSHARE:?}/scripts/tqpdflatex.sh" results/LQtaub-lephad-${REGION}-${TASK}/summary.tex
 
-REGION="VR"
-TASK="FF-os"
-visualize.py LQtaub-lephad/config/SR/master/visualize-${REGION}-${TASK}.cfg 
-source "${CAFANALYSISSHARE:?}/scripts/tqpdflatex.sh" results/LQtaub-lephad-${REGION}-${TASK}/summary.tex
-
-REGION="ZCR"
-TASK="FF-os"
-visualize.py LQtaub-lephad/config/SR/master/visualize-${REGION}-${TASK}.cfg 
-source "${CAFANALYSISSHARE:?}/scripts/tqpdflatex.sh" results/LQtaub-lephad-${REGION}-${TASK}/summary.tex
+#REGION="VR"
+#TASK="FF-os"
+#visualize.py LQtaub-lephad/config/SR/master/visualize-${REGION}-${TASK}.cfg 
+#source "${CAFANALYSISSHARE:?}/scripts/tqpdflatex.sh" results/LQtaub-lephad-${REGION}-${TASK}/summary.tex
