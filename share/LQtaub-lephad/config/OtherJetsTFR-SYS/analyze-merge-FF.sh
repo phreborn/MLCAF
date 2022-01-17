e0408a06489abff57340fc9c6b6efa7307c23de0
Hanfei Ye <hanfei.ye@cern.ch>
2022-01-17 14:37:54 +0800

    add SYS for OtherJetsTFR

---
+++ share/LQtaub-lephad/config/OtherJetsTFR-SYS/analyze-merge-FF.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# common config
+SAMPLEBASE="sampleFolders/analyzed/samples-analyzed-LQtaub-lephad-OtherJetsTFR-FF"
+
+# handle arguments
+if [ -z "$*" ]; then
+    echo "No arguments provided!"
+    return
+fi
+echo "Accepting arguments: $*"
+
+# loop execute
+for ARG in "$@"; do
+    echo ""
+    if [ "${ARG}" == "ALL" ]; then
+        # merge all individual sys together with nominal
+        echo "Merging ALL systematics!"
+        tqmerge -t analyze \
+            "${SAMPLEBASE}"_sys_*.root "${SAMPLEBASE}".root \
+            -o "${SAMPLEBASE}"_sys.root
+    else
+        # merge individual sys
+        echo "Merging systematic: ${ARG}"
+        tqmerge -t analyze \
+            batchOutput/unmerged_OtherJetsTFR-FF-SYS-"${ARG}"/*.root \
+            -o "${SAMPLEBASE}"-SYS-"${ARG}".root \
+            --downmerge generalize \
+            --patch LQtaub-lephad/config/common/patches/downmerge.txt \
+            --patch LQtaub-lephad/config/common/patches/trimmingAfterPooling.txt
+    fi
+done
---
+++ share/LQtaub-lephad/config/OtherJetsTFR-SYS/analyze-submit-FF.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+REGION="SYS"
+
+#JOBLIST="jobs_SYS-SR-FF_c16a.txt jobs_SYS-SR-FF_c16d.txt jobs_SYS-SR-FF_c16e.txt"
+
+# handle arguments
+if [ -z "$*" ]; then
+    echo "No arguments provided!"
+    return
+fi
+echo "Accepting arguments: $*"
+
+
+# loop execute
+for ARG in "$@"; do
+    echo ""
+    echo "Submitting systematic: ${ARG}"
+
+    # config
+    CONFIG="LQtaub-lephad/config/OtherJetsTFR-${REGION}/master/analyze-systematics-${ARG}.cfg"
+    IDENTIFIER="OtherJetsTFR-FF-${REGION}-${ARG}"
+    JOBLIST="jobs_SYS-SR-FF-${ARG}_c16a.txt jobs_SYS-SR-FF-${ARG}_c16d.txt jobs_SYS-SR-FF-${ARG}_c16e.txt"
+
+    # execute
+    source "${CAFANALYSISSHARE:?}/LQtaub-lephad/config/common/submit.sh" "${CONFIG}" "${IDENTIFIER}" "${JOBLIST}"
+done
+
+
+
