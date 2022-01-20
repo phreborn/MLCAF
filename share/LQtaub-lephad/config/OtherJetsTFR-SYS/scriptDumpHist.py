e2d4b24828c898ed3e7f60941f0bae8c579d69eb
Hanfei Ye <hanfei.ye@cern.ch>
2022-01-20 14:29:19 +0800

    update SYS for TCR, FTCR, SSR

---
+++ share/LQtaub-lephad/config/OtherJetsTFR-SYS/scriptDumpHist.py
@@ -0,0 +1,94 @@
+def create_cmd_log_list(input_files, campaigns, channels, variations):
+  cmd_list = []
+  log_list = []
+
+  for input_file in input_files:
+    for campaign in campaigns:
+      for channel in channels:
+        variation_file = variations[input_file.split("/")[-1]]
+        cmd = 'python scripts/dumpHist_lq.py {0} {1} {2} {3} {4} {5}'.format(input_file, campaign, channel, variation_file, args.version, args.coupling)
+        cmd_list.append(cmd)
+        syst_name = input_file.split('.')[0].split('-')[-1]
+
+        if channel == '[ehad+muhad]': 
+          channel = 'lephad'
+        
+        log_dir = 'logs/dump_hist/{0}/{1}/{2}_l{3}'.format(campaign, channel, args.version, args.coupling)
+        if not os.path.isdir(log_dir):
+          os.makedirs(log_dir)
+        log = '{0}/{1}_{2}_{3}_l{4}_{5}.log'.format(log_dir, campaign, channel, args.version, args.coupling, syst_name)
+        log_list.append(log)
+
+  return cmd_list, log_list
+        
+
+if __name__ == '__main__':
+
+  import os
+  import subprocess
+  import argparse
+  import commands
+  from JobHandler import LocalJobHandler
+
+  """Script to dump histograms"""
+
+  parser = argparse.ArgumentParser(description='Dump histogram for statistical analysis.')
+  parser.add_argument('version', metavar='VERSION', type=str,
+            default="st300",
+            help='version to be used')
+  parser.add_argument('coupling', metavar='COUPLING', type=str,
+            default="1_0",
+            help='coupling to be used')
+  args = parser.parse_args()
+
+
+  # colours
+  ENDC = '\033[0m'
+  WARNING = '\033[93m'
+  BWHITE = '\033[1;37m'
+
+  # Get the usual campaigns and channels
+  campaigns = ['c16ade']
+  channels = ['ehad','muhad'] 
+
+  variations = {
+                'samples-analyzed-LQtaub-lephad-OtherJetsTFR-FF-SYS-NOMINAL.root': None,
+                'samples-analyzed-LQtaub-lephad-OtherJetsTFR-FF-SYS-MultiJetsLFR.root': 'LQtaub-lephad/auxData/variations/systematics-Fake-MJ.txt',
+                #'samples-analyzed-LQtaub-lephad-OtherJetsTFR-FF-SYS-MCFakes.root': 'LQtaub-lephad/auxData/variations/systematics-Fake-MC.txt',
+                'samples-analyzed-LQtaub-lephad-OtherJetsTFR-FF-SYS-Top_Reweight.root': 'LQtaub-lephad/auxData/variations/systematics-Top-Reweight.txt',
+                #'samples-analyzed-LQtaub-lephad-OtherJetsTFR-FF-SYS-Top_Residual.root': 'LQtaub-lephad/auxData/variations/systematics-Top-Residual.txt',
+                #'samples-analyzed-LQtaub-lephad-OtherJetsTFR-FF-SYS-CP_jet_p4.root': 'LQtaub-lephad/auxData/variations/systematics-CP-jet-p4.txt',
+                #'samples-analyzed-LQtaub-lephad-OtherJetsTFR-FF-SYS-CP_lep_p4.root': 'LQtaub-lephad/auxData/variations/systematics-CP-lep-p4.txt',
+                #'samples-analyzed-LQtaub-lephad-OtherJetsTFR-FF-SYS-CP_lep_weight.root': 'LQtaub-lephad/auxData/variations/systematics-CP-lep-weight.txt',
+                #'samples-analyzed-LQtaub-lephad-OtherJetsTFR-FF-SYS-CP_tau_weight.root': 'LQtaub-lephad/auxData/variations/systematics-CP-tau-weight.txt',
+                #'samples-analyzed-LQtaub-lephad-OtherJetsTFR-FF-SYS-CP_other_weight.root': 'LQtaub-lephad/auxData/variations/systematics-CP-other-weight.txt',
+                #'samples-analyzed-LQtaub-lephad-OtherJetsTFR-FF-SYS-Theory_Top_PS.root': 'LQtaub-lephad/auxData/variations/systematics-Theory-Top-PS.txt',
+                #'samples-analyzed-LQtaub-lephad-OtherJetsTFR-FF-SYS-Theory_Top_ME.root': 'LQtaub-lephad/auxData/variations/systematics-Theory-Top-ME.txt',
+                #'samples-analyzed-LQtaub-lephad-OtherJetsTFR-FF-SYS-Theory_Top_ISRup.root': 'LQtaub-lephad/auxData/variations/systematics-Theory-Top-ISRup.txt',
+                #'samples-analyzed-LQtaub-lephad-OtherJetsTFR-FF-SYS-Theory_Top_ISRdo.root': 'LQtaub-lephad/auxData/variations/systematics-Theory-Top-ISRdo.txt',
+                #'samples-analyzed-LQtaub-lephad-OtherJetsTFR-FF-SYS-Theory_Top_FSRup.root': 'LQtaub-lephad/auxData/variations/systematics-Theory-Top-FSRup.txt',
+                #'samples-analyzed-LQtaub-lephad-OtherJetsTFR-FF-SYS-Theory_Top_FSRdo.root': 'LQtaub-lephad/auxData/variations/systematics-Theory-Top-FSRdo.txt',
+                #'samples-analyzed-LQtaub-lephad-OtherJetsTFR-FF-SYS-Theory_Zjets.root': 'LQtaub-lephad/auxData/variations/systematics-Theory-Zjets.txt',
+                }  
+
+  # Get the input files
+  
+  directory = '/publicfs/atlas/atlasnew/higgs/hh2X/yehf/LQ/OtherJetsTFR-SYS/'
+  files = [directory+i for i in os.listdir(directory) if i in variations.keys()]
+
+  print(BWHITE + 'The following files histograms will be dumped:\n\n' + "\n".join(files) + ENDC)
+  print("\n")
+
+  cmd_list, log_list = create_cmd_log_list(files, campaigns, channels, variations)  
+  cmd_log = zip(cmd_list, log_list)
+  NCORES = 8
+  
+  if NCORES == 1:
+    for cmd in cmd_list:
+      os.system(cmd)
+  else:
+    local_job_handler = LocalJobHandler(cmd_log, NCORES)
+    local_job_handler.run()
+
+  print(BWHITE + 'Jobs Finished check logs and use merging script' + ENDC)
+
---
+++ share/LQtaub-lephad/config/OtherJetsTFR-SYS/scriptMergeHist.py
@@ -0,0 +1,50 @@
+if __name__ == '__main__':
+
+  import os
+  import argparse
+
+  parser = argparse.ArgumentParser(description='Merge histogram for statistical analysis.')
+  parser.add_argument('version', metavar='VERSION', type=str,
+            default="st300",
+            help='version to be used')
+  parser.add_argument('coupling', metavar='COUPLING', type=str,
+            default="1_0",
+            help='coupling to be used')
+  args = parser.parse_args()
+
+
+  "Merge the histograms"
+  campaigns = ['c16ade']
+  channels = ['ehad', 'muhad']
+  NCORES = 8
+  variable = 'St'
+  regions = []
+  regions.extend(['SROSBtagLowBJetPt', 'SROSBtagHighBJetPt'])
+  #regions.extend(['VROSBtagLowBJetPt', 'VROSBtagHighBJetPt'])
+  #regions.extend(['VROSBtag'])
+  #regions.extend(['TCROSBtag'])
+  #regions.extend(['TFROSBtag'])
+  #regions.extend(['TVROSBtag'])
+  #regions.extend(['LFROSBtag'])
+  #regions.extend(['WFROSBtag'])
+  #regions.extend(['SROSBtag'])
+
+  root_file_name = '13TeV_OneLepton_CUT_'
+  for campaign in campaigns:
+    for channel in channels:
+      for region in regions:
+        merge_to = root_file_name
+        if 'Btag' in region:
+          merge_to += '1tag0jet_0ptv_'+region+'_'
+        else:
+          merge_to += '0tag0jet_0ptv_'+region+'_'
+        if 'ehad' in channel:
+          merge_to += 'ElHad_'
+        elif 'muhad' in channel:
+          merge_to += 'MuHad_' 
+        elif 'lephad' in channel:
+          merge_to += 'LepHad_'
+        merge_to += variable + '.root'
+        cmd = 'hadd -f -j {0} dumpHist_{5}_l{6}/{1} dumpHist_{5}_l{6}/*/{2}/{3}/{2}_{4}_{3}.root'.format(NCORES, merge_to, campaign, channel, region, args.version, args.coupling) 
+        os.system(cmd)
+
