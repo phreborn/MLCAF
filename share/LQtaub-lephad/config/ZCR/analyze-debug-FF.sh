d5314d602827179fd993137973edd2fad9d60ffb
Hanfei Ye <hanfei.ye@cern.ch>
2022-03-15 20:03:14 +0800

    Add ZCR

---
+++ share/LQtaub-lephad/config/ZCR/analyze-debug-FF.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+REGION="ZCR"
+TASK="FF"
+analyze.py LQtaub-lephad/config/${REGION}/master/analyze-${TASK}.cfg --debug \
+      --restrict /bkg/?/c16d/mcFakeCorrected/Top/single \
---
+++ share/LQtaub-lephad/config/ZCR/analyze-merge-FF.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+REGION="ZCR"
+TASK="FF"
+tqmerge -o sampleFolders/analyzed/samples-analyzed-LQtaub-lephad-${REGION}-${TASK}-NOMINAL.root -t analyze batchOutput/unmerged_${REGION}-${TASK}/*.root --downmerge generalize --patch LQtaub-lephad/config/common/patches/downmerge.txt --patch LQtaub-lephad/config/common/patches/trimmingAfterPooling.txt
+
+
