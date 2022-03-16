cfb15a1ed03f85a623043743c2d0f3af70ba1f3b
Hanfei Ye <hanfei.ye@cern.ch>
2022-03-16 15:36:14 +0800

    use new parameterization for fake tau correction

--- share/LQtaub-lephad/config/OtherJetsSSR/analyze-merge-FF-closure.sh
+++ share/LQtaub-lephad/config/OtherJetsSSR/analyze-merge-FF-closure.sh
@@ -3,4 +3,5 @@
 REGION="OtherJetsSSR"
 TASK="FF-closure"
 
-tqmerge -o sampleFolders/analyzed/samples-analyzed-LQtaub-lephad-${REGION}-${TASK}.root -t analyze batchOutput/unmerged_${REGION}-${TASK}/*.root
+tqmerge -o sampleFolders/analyzed/samples-analyzed-LQtaub-lephad-${REGION}-${TASK}.root -t analyze batchOutput/unmerged_${REGION}-${TASK}/*.root \
+    `find ./batchOutput/unmerged_OtherJetsSSR-FF -name "*.root" | grep -v mcFakeCorrected`
