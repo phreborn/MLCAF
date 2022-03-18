bdd526c3b75dd7889e7dd421dc036ed80e8c8f0b
Hanfei Ye <hanfei.ye@cern.ch>
2022-03-18 14:04:45 +0800

    split single top into tW, s-channel and t-channel

--- share/LQtaub-lephad/config/TCR/analyze-debug-TCR-SF.sh
+++ share/LQtaub-lephad/config/TCR/analyze-debug-TCR-SF.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 analyze.py LQtaub-lephad/config/TCR/master/analyze-TCR-SF.cfg --debug \
-      --restrict /bkg/?/c16a/mcReal/Top/ttbar/nominal
+      --restrict /bkg/?/c16a/mcReal/Top/single/nominal/tW
