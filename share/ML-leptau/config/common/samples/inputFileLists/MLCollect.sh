b76a63e3a3a125b65c40e0af4fe2e632e58e96da
chen hui run <huirun@lxslc710.ihep.ac.cn>
2022-07-23 11:50:08 +0800

    sample list, normalization preliminary completed

--- share/ML-leptau/config/common/samples/inputFileLists/MLCollect.sh
+++ share/ML-leptau/config/common/samples/inputFileLists/MLCollect.sh
@@ -1,13 +1,14 @@
 #!/bash/bin
 
+dir=/publicfs/atlas/atlasnew/higgs/hh2X/huirun/multilepton/leptau/MLntuple/MVA
 dir=/publicfs/atlas/atlasnew/higgs/hh2X/huirun/multilepton/leptau/MLntuple/gn2
 
-for camp in mc16a
+for camp in mc16e
 do
-  ls ${dir}/${camp}/*root > FileList_nom_${camp}.txt
+  ls ${dir}/${camp}/*root | grep -v user > FileList_nom_${camp}.txt
 done
 
-for camp in data1516
+for camp in data18
 do
   ls ${dir}/${camp}/*root > FileList_${camp}.txt
 done
